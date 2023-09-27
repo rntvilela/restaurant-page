31f73751c803d2163cc22de41b549ccf4cfec85c
rntvilela <renatovilela55@gmail.com>
2023-09-27 20:57:24 -0300

    Add header

---
+++ main.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";document.addEventListener("DOMContentLoaded",(()=>{!function(){const e=document.getElementById("content"),t=document.createElement("header"),n=document.createElement("h1");n.textContent="Nordic Cousine",t.appendChild(n),e.appendChild(t)}()}))})();
+//# sourceMappingURL=main.js.map
