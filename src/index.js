bef91cfeef2159e9605b33cefdcb0973c36709e4
rntvilela <renatovilela55@gmail.com>
2023-10-05 22:09:38 -0300

    Add styles.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import './css/styles.css';
+
 import { navbar } from './navbar.js'
 import { home } from './home.js'
 import { menu } from './menu.js'
 import { contact } from './contact.js'
-//import { footer } from './footer.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 	navbar();
