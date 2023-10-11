7984f2ee4e4462a66076b31a6068adb75e7011ba
rntvilela <renatovilela55@gmail.com>
2023-10-10 22:13:59 -0300

    Add menu

--- src/menu.js
+++ src/menu.js
@@ -3,8 +3,28 @@ function menu() {
 	const menu    =      document.createElement('div');
 
 	menu.id = 'menu';
-	menu.textContent = 'Menu Page';
 	menu.style.display = 'none';
+	
+	const menu_title = document.createElement('div');
+	menu_title.id = 'menu_tittle';
+	menu_title.textContent = 'Menu';
+	menu.appendChild(menu_title);
+
+	const appetizers = document.createElement('div');
+	appetizers.id = 'appetizers';
+	const appetizers_tittle = document.createElement('div');
+	appetizers_tittle.id = 'appetizers_tittle';
+	appetizers_tittle.textContent = 'Appetizers';
+	appetizers.appendChild(appetizers_tittle);
+	
+	let appetizers_items = ["1 - Smoked Salmon Soup - $18.50", "2 - Pickled Herring with New Potatoes - $15.80", "3 - Beet and Apple Salad - $14.90"];
+	
+	for (let i = 0; i < appetizers_items.length; i++) {
+		const li = document.createElement('div');
+		li.appendChild(document.createTextNode(appetizers_items[i]));
+		appetizers.appendChild(li);
+	}
+	menu.appendChild(appetizers);
 	content.appendChild(menu);
 }
 
